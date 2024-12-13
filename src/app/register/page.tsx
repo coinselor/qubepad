e16d247d33c0b584205fd7b5fc73d3aac44fbba6
coinselor <coinselor@protonmail.com>
2024-12-13 18:22:22 +0000

    fix: add min-h-screen to RegisterPage

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,7 @@ import PillarsData from "@/components/PillarsData";
 
 export default function RegisterPage() {
 	return (
-		<main className="md:px-8 w-full md:py-12 bg-zinc-950">
+		<main className="md:px-8 w-full md:py-12 min-h-screen bg-zinc-950">
 			<Dashboard />
 			<PillarsData />
 		</main>
